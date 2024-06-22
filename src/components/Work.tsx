746a241f338d295b664813285acb0d49805bbb08
MoritzBru <moritz_brunnengraeber@hotmail.com>
2024-06-22 11:01:10 +0200

    fix: work layout

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -8,20 +8,26 @@ import MaybeLink from './helper/MaybeLink.js';
 const Position = (position: Exclude<ResumeSchema['work'], undefined>[number]) => (
   <article>
     <header class="flex justify-between">
-      <MaybeLink url={position.url}>
-        <span class="font-bold">{position.name}</span>
-      </MaybeLink>
+      {position.position && <p class="font-bold">{position.position}</p>}
       {position.startDate && <TimeRange startDate={position.startDate} endDate={position.endDate} />}
     </header>
     <div>
       <div class="flex justify-between">
-        {position.position && <p class="">{position.position}</p>}
+        <MaybeLink url={position.url}>
+          <span class="font-bold opacity-75">{position.name}</span>
+        </MaybeLink>
         {position.location && <p class="opacity-75">{position.location}</p>}
       </div>
+      { position.description && (<p class="font-light leading-tight">{position.description}</p>)}
       { position.summary && (
         <div class="mt-1 text-justify text-pretty font-light leading-tight">
           {new RawHtml(md2html(position.summary))}
         </div>
+      )}
+      { position.highlights?.length && (
+        <ul class="ml-4 mt-1 list-disc list-outside font-light leading-tight marker:text-secondary">
+          {position.highlights.map((highlight) => <li>{highlight}</li>)}
+        </ul>
       )}
     </div>
   </article>
