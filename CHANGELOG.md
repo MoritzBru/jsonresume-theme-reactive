f5d003b9632c96246faf48ff932eabc4c7250dfb
MoritzBru <moritz_brunnengraeber@hotmail.com>
2024-06-27 16:23:40 +0200

    feat: tweak dark colors

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/) and this 
 
 - Monospace font "Space Mono" used for timeranges
 
+### Changed
+
+- Tweaked dark colors
+
 ## [0.2.0] - 2024-06-25
 
 ### Added
--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -5,7 +5,7 @@ import { md2html } from '../utils/converte';
 const Summary = ({ basics, className }: { basics: ResumeSchema['basics']; className?: string }) => (
   <>
     {basics?.summary && (
-      <section class={['bg-primary-200 p-4 text-justify text-pretty text-black dark:bg-primary-700 dark:text-white', className].filter(Boolean).join(' ')}>
+      <section class={['bg-primary-200 p-4 text-justify text-pretty text-black dark:bg-primary-800 dark:text-white', className].filter(Boolean).join(' ')}>
         {new RawHtml(md2html(basics.summary))}
       </section>
     )}
